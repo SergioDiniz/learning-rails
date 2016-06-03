2d8f059eeff02e14bc2e645acc28ff5836343ceb
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-02 21:58:02 -0300

    new solution

--- colchonet/app/assets/javascripts/room.js
+++ colchonet/app/assets/javascripts/room.js
@@ -18,30 +18,38 @@ $(function(){
 		replaceWith($('<i/>').addClass(icon_class).css('color', color));
 	};
 
+});
 
-	// estrelas
-	function highlightStars(elem) {
-	   elem.parent().children('label').removeClass('toggled');
-	   elem.addClass('toggled').prevAll('label').addClass('toggled');
-	}
 
-	highlightStars($('.review input:checked + label'));
 
-	var $stars = $('.review input:enabled ~ label');
+// problema com turbolinks
+$(document).on('ready page:load', function () {
+  	// Actions to do
+	$(function(){
+		
+		// estrelas
+		function highlightStars(elem) {
+		   elem.parent().children('label').removeClass('toggled');
+		   elem.addClass('toggled').prevAll('label').addClass('toggled');
+		}
 
-	$stars.on('mouseenter', function() {
-		highlightStars($(this));
-	});
-
-	$stars.on('mouseleave', function() {
 		highlightStars($('.review input:checked + label'));
+
+		var $stars = $('.review input:enabled ~ label');
+
+		$stars.on('mouseenter', function() {
+			highlightStars($(this));
+		});
+
+		$stars.on('mouseleave', function() {
+			highlightStars($('.review input:checked + label'));
+		});
+
+		$('.review input').on('change', function() {
+			$stars.off('mouseenter').off('mouseleave').off('click');
+			$(this).parent('form').submit();
+		});
 	});
-
-	$('.review input').on('change', function() {
-		$stars.off('mouseenter').off('mouseleave').off('click');
-		$(this).parent('form').submit();
-	});
-
-
 
 });
+
