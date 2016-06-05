07651664ebeda13162e3d4803d59dbe9bb0a0371
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-05 11:36:19 -0300

    migrate postgresql

--- colchonet/db/migrate/20160531115047_create_reviews.rb
+++ colchonet/db/migrate/20160531115047_create_reviews.rb
@@ -8,8 +8,6 @@ class CreateReviews < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :reviews, :user_id
-    add_index :reviews, :room_id
     add_index :reviews, [:user_id, :room_id], unique: true
 
   end
